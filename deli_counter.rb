a9891c03a75ee8776c42e4ba877534f71cc13f92
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 16:32:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
-  
+  if katz_deli.length == 0
+    puts "The line is currently empty."
 end
 def take_a_number(array, name)
   counter = 0
