91cad8dac5a8f0326ffe4649ef3ee4cb5a3a1ac2
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 16:39:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,21 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
-    def take_a_number(array, name)
-    counter = 0
-    array.each do
-    "Welcome, #{name}. You are number #{array[counter+1]} in line."
-    counter += 1
-    end
-  end
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{katz_deli}"
+    counter = 0
+    katz_deli.each do
+    puts "The line is currently: #{number[counter+1]}. #{katz_deli[counter+1]}"
+    counter += 1
+    end
   end
 end
 
+def take_a_number(array, name)
+  counter = 0
+  array.each do
+  "Welcome, #{name}. You are number #{array[counter+1]} in line."
+  counter += 1
+  end
+end
