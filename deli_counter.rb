e56e1062a5a1bd716a98ff168016c8e42be2965c
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 16:48:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,9 +14,13 @@ def line(katz_deli)
 end
 
 def take_a_number(array, name)
+  if array.length == 0
+    puts "There is nobody in line."
+  else
   counter = 0
   array.each do
-  "Welcome, #{name}. You are number #{array[counter+1]} in line."
-  counter += 1
+    puts "Welcome, #{name}. You are number #{array[counter+1]} in line."
+    counter += 1
+    end
   end
 end
