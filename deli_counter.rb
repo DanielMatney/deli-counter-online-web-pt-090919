818f9d0b98a8ea7277c59b660e18832f0d238282
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 16:30:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  
+end
+def take_a_number(array, name)
+  counter = 0
+  array.each do
+  "Welcome, #{name}. You are number #{array[counter+1]} in line."
+  counter += 1
+  end
+end
